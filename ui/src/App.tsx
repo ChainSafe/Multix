fa76d953a7778c7c95b466e2bc8d68852606c828
Thibaut Sardan <github@thib.top>
2023-03-20 11:34:27 +0000

    nits header

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Creation, Home } from "./pages"
-import { Header } from "./components/Header"
+import Header from "./components/Header"
 import Container from "@mui/material/Container"
 import { UserSpace } from "./components/UserSpace"
 import { AccountContextProvider } from "./contexts/AccountsContext"
