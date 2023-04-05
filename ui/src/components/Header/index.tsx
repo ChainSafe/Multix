eb639049ff4ba7451c2d4a887c5d455c72ded590
Thibaut Sardan <github@thib.top>
2023-04-05 16:24:38 +0200

    disable network selection

--- ui/src/components/Header/index.tsx
+++ ui/src/components/Header/index.tsx
@@ -46,7 +46,7 @@ const Header = ({ className }: Props) => {
               <MultiProxySelection />
             </>
           )}
-          <NetworkSelection />
+          {/* <NetworkSelection /> */}
         </Toolbar>
       </Container>
     </AppBar>
