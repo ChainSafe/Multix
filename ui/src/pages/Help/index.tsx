a136ff3f2481e64e021b29ea30ee8128ee235676
Thibaut Sardan <github@thib.top>
2023-04-19 13:13:54 +0100

    flow

--- ui/src/pages/Help/index.tsx
+++ ui/src/pages/Help/index.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components"
-import { Alert, Box, Chip } from "@mui/material"
-import { MultiProxy, useMultiProxy } from "../contexts/MultiProxyContext"
-import AccountDisplay from "../components/AccountDisplay"
+import { Alert, Box } from "@mui/material"
+import { MultiProxy, useMultiProxy } from "../../contexts/MultiProxyContext"
+import AccountDisplay from "../../components/AccountDisplay"
 import { useMemo } from "react"
-import { AccountBadge } from "../types"
-import MultisigCompactDisplay from "../components/MultisigCompactDisplay"
+import { AccountBadge } from "../../types"
+import MultisigCompactDisplay from "../../components/MultisigCompactDisplay"
+import 'reactflow/dist/style.css';
+import Overview from "./Overview"
 
 interface Props {
   className?: string
@@ -13,14 +15,14 @@ interface Props {
 const getMultisigInfo = (multisig: MultiProxy['multisigs'][0]) => {
   const signatoryLegnth = multisig.signatories?.length
   return (
-    <p>
+    <div>
       This account is a multisig comprised of {signatoryLegnth} signatories. Its threshold is {multisig.threshold} out of {signatoryLegnth}. It means that for every multisig proposal, at least {multisig?.threshold} signatories need to approve it before it can be executed.
       {signatoryLegnth === multisig.threshold && (
         <Alert className="errorBox" severity="error">
           The fact that the threshold is equal to to the amount of signatories is dangerous in the sense that if any of the signatory looses access to its account, the funds on the multisig account will be inaccessible.
         </Alert>
       )}
-    </p>
+    </div>
   )
 }
 
@@ -41,24 +43,24 @@ const Help = ({ className }: Props) => {
               withName
             />
             <div>
-              <p>
+              <div>
                 A pure account, also called pure proxy is a very specific type of account that has no private key.
                 It should be the externally facing account for a company or a DAO. It should be the account that holds funds. This is an account that will not change regardless of who controls it.
-              </p>
-              <p>
+              </div>
+              <div>
                 In this specific case it is controlled by {selectedMultiProxy?.multisigs.length} {hasSeveralMultisigs ? "accounts" : "account"}:
                 <ul className="accountList">
                   {selectedMultiProxy?.multisigs.map(({ address }) => (
                     <li key={address}><AccountDisplay address={address} badge={AccountBadge.MULTI} /></li>
                   ))}
                 </ul>
-              </p>
+              </div>
             </div>
           </Box>
           <Box className="sectionWrapper">
             <h1>{hasSeveralMultisigs ? "Multisigs" : "Multisig"}</h1>
             {selectedMultiProxy?.multisigs.map((multisig) => (
-              <Box className="multisigWithProxy">
+              <Box className="multisigWithProxy" key={multisig.address}>
                 <MultisigCompactDisplay
                   address={selectedMultiProxy?.multisigs[0].address || ""}
                   expanded={false}
@@ -83,6 +85,8 @@ const Help = ({ className }: Props) => {
           </Alert>
         </Box>
       )}
+      <h1>Overview</h1>
+      <Overview />
     </Box>
   )
 }
