65b98b61db21213976dfe996feef89a2428bf030
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2023-04-25 22:27:25 +0200

    Bump dependancies and resolves resolution issue with graphql-codegen (#102)

--- ui/types-and-hooks.tsx
+++ ui/types-and-hooks.tsx
@@ -424,8 +424,10 @@ export enum ProxyType {
   CancelProxy = 'CancelProxy',
   Governance = 'Governance',
   IdentityJudgement = 'IdentityJudgement',
+  NominationPools = 'NominationPools',
   NonTransfer = 'NonTransfer',
-  Society = 'Society'
+  Staking = 'Staking',
+  SudoBalances = 'SudoBalances'
 }
 
 export type Query = {
