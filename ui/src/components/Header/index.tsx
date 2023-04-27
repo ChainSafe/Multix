9bca084388a8b8e0cf1f84cb5335f45de8a47c43
Thibaut Sardan <github@thib.top>
2023-04-27 23:28:00 +0100

    hide help when no multisig yet

--- ui/src/components/Header/index.tsx
+++ ui/src/components/Header/index.tsx
@@ -1,22 +1,24 @@
-import {Box, Button, IconButton, Toolbar, Typography} from "@mui/material"
+import { Box, Button, IconButton, Toolbar, Typography } from "@mui/material"
 import styled from "styled-components"
-import * as React from "react";
+import { useMemo } from "react";
 import MuiAppBar from '@mui/material/AppBar';
 import MultiProxySelection from "../MultiProxySelection";
-import {useAccounts} from "../../contexts/AccountsContext";
-import {Link} from "react-router-dom";
+import { useAccounts } from "../../contexts/AccountsContext";
+import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
-import {ROUTES} from "../../constants";
+import { ROUTES } from "../../constants";
+import { useMultiProxy } from "../../contexts/MultiProxyContext";
 // import NetworkSelection from "../NetworkSelection"
 
 interface Props {
-    className?: string
-    handleDrawerOpen: () => void,
+  className?: string
+  handleDrawerOpen: () => void,
 }
 
-const Header = ({className, handleDrawerOpen}: Props) => {
-  const {accountList} = useAccounts();
-  const isAccountConnected = !!accountList?.length;
+const Header = ({ className, handleDrawerOpen }: Props) => {
+  const { accountList } = useAccounts();
+  const isAccountConnected = useMemo(() => !!accountList?.length, [accountList])
+  const { multiProxyList } = useMultiProxy()
 
   return (
     <MuiAppBar position="fixed" className={className}>
@@ -25,22 +27,26 @@ const Header = ({className, handleDrawerOpen}: Props) => {
           variant="h6"
           noWrap
         >
-            Multix
+          Multix
         </TypographyStyled>
         {isAccountConnected && (
           <BoxStyled>
-            {ROUTES.map(({url, name}) => (
-              <Button
-                component={Link}
-                to={url}
-                className="buttonHeader"
-              >
-                {name}
-              </Button>
+            {ROUTES.map(({ url, name, isDisplayWhenNoMultiProxy }) => (
+              multiProxyList.length > 0 || isDisplayWhenNoMultiProxy
+                ? (
+                  <Button
+                    component={Link}
+                    to={url}
+                    className="buttonHeader"
+                  >
+                    {name}
+                  </Button>
+                )
+                : null
             ))}
           </BoxStyled>
         )}
-        <MultiProxySelection/>
+        <MultiProxySelection />
         {/*/!*<NetworkSelection />*!/*/}
         {isAccountConnected && (
           <IconButtonStyled
@@ -49,7 +55,7 @@ const Header = ({className, handleDrawerOpen}: Props) => {
             edge="end"
             onClick={handleDrawerOpen}
           >
-            <MenuIcon/>
+            <MenuIcon />
           </IconButtonStyled>
         )}
       </Toolbar>
@@ -57,7 +63,7 @@ const Header = ({className, handleDrawerOpen}: Props) => {
   )
 }
 
-const BoxStyled = styled(Box)(({theme}) => `
+const BoxStyled = styled(Box)(({ theme }) => `
     display: none;
     
     @media (min-width: ${theme.breakpoints.values.sm}px) {
@@ -71,14 +77,14 @@ const TypographyStyled = styled(Typography)`
   flex-grow: 1;
 `
 
-const IconButtonStyled = styled(IconButton)(({theme}) => `
+const IconButtonStyled = styled(IconButton)(({ theme }) => `
     display: block;
     @media (min-width: ${theme.breakpoints.values.sm}px) {
         display: none;
     }
 `)
 
-export default styled(Header)(({theme}) => `
+export default styled(Header)(({ theme }) => `
   .buttonHeader {
     color: ${theme.palette.primary.white};
     text-align: center;
