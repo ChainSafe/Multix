a73c7b3543d0701468ec89237c65e39b52128864
Anton Lykhoyda <lykhoyda@gmail.com>
2023-05-10 11:13:13 +0200

    fix lint

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,16 +1,17 @@
-import "./App.css"
-import { AccountContextProvider } from "./contexts/AccountsContext"
-import { ApiContextProvider } from "./contexts/ApiContext"
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { MultiProxyContextProvider } from "./contexts/MultiProxyContext"
-import { ToastContextProvider } from "./contexts/ToastContext"
-import { theme } from "./theme"
-import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
-import { AccountNamesContextProvider } from "./contexts/AccountNamesContext"
-import { NetworkContextProvider } from "./contexts/NetworkContext"
+import "./App.css";
+import { AccountContextProvider } from "./contexts/AccountsContext";
+import { ApiContextProvider } from "./contexts/ApiContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MultiProxyContextProvider } from "./contexts/MultiProxyContext";
+import { ToastContextProvider } from "./contexts/ToastContext";
+import { theme } from "./theme";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { AccountNamesContextProvider } from "./contexts/AccountNamesContext";
+import { NetworkContextProvider } from "./contexts/NetworkContext";
 import MainLayout from "./components/layout/Main";
+
 const App = () => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   return (
     <MuiThemeProvider theme={theme}>
@@ -30,7 +31,7 @@ const App = () => {
         </NetworkContextProvider>
       </ToastContextProvider>
     </MuiThemeProvider>
-  )
-}
+  );
+};
 
 export default App;
