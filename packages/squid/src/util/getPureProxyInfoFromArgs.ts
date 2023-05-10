973f0f03beda4d5ddd3cf609522bcddb4aa66135
Thibaut Sardan <github@thib.top>
2023-05-10 15:34:28 +0200

    nits

--- packages/squid/src/util/getPureProxyInfoFromArgs.ts
+++ packages/squid/src/util/getPureProxyInfoFromArgs.ts
@@ -7,9 +7,8 @@ import { getProxyTypeFromRaw } from "./getProxyTypeFromRaw";
 export const getPureProxyInfoFromArgs = (item: EventItem<"Proxy.PureCreated", typeof dataEvent['data']>) => {
 
   const { pure, who, proxyType } = item.event.args
-  // pure proxy creation delay, they have a disambiguationIndex
+  // pure proxy have no creation delay, they have a disambiguationIndex
   const delay = 0
-  // pure proxy creation always have the any type
   const _type = getProxyTypeFromRaw(proxyType.__kind)
   const _who = encodeAddress(who, env.prefix)
   const _pure = encodeAddress(pure, env.prefix)
@@ -19,7 +18,7 @@ export const getPureProxyInfoFromArgs = (item: EventItem<"Proxy.PureCreated", ty
     id,
     who: _who,
     pure: _pure,
-    delay: delay,
+    delay,
     type: _type
   })
 }
