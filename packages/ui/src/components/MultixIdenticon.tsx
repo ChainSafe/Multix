19d04d00e8ff43db5c5c7a5ca5cf5e309125f462
Digvijay Pundir (DJ) <61174028+dj-pundir@users.noreply.github.com>
2023-05-22 22:01:16 +0530

    Add tooltip for identicon (#149)
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>

---
+++ packages/ui/src/components/MultixIdenticon.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useState } from 'react'
+import Tooltip from '@mui/material/Tooltip'
+import { ICON_SIZE, ICON_THEME } from '../constants'
+import Identicon from '@polkadot/react-identicon'
+import { IconTheme } from '@polkadot/react-identicon/types'
+import { styled } from '@mui/material/styles'
+
+const DEFAULT_PLACEMENT = 'top'
+const DEFAULT_TITLE = 'Address copied!'
+const DEFAULT_AUTO_HIDE_DURATION = 2000
+
+interface Props {
+  value?: string
+  theme?: IconTheme
+  size?: number
+  className?: string
+}
+const MultixIdenticon = ({ value, theme = ICON_THEME, size = ICON_SIZE, className }: Props) => {
+  const [open, setOpen] = useState(false)
+  const handleTooltipClose = useCallback(() => setOpen(false), [])
+  const handleTooltipOpen = useCallback(() => setOpen(true), [])
+
+  return (
+    <Tooltip
+      onClose={handleTooltipClose}
+      open={open}
+      title={DEFAULT_TITLE}
+      placement={DEFAULT_PLACEMENT}
+      leaveDelay={DEFAULT_AUTO_HIDE_DURATION}
+    >
+      <TooltipIconStyled>
+        <Identicon
+          onCopy={handleTooltipOpen}
+          value={value}
+          theme={theme}
+          size={size}
+          className={className}
+        />
+      </TooltipIconStyled>
+    </Tooltip>
+  )
+}
+
+const TooltipIconStyled = styled('div')`
+  display: inherit;
+  line-height: 0;
+`
+
+export default MultixIdenticon
