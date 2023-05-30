1d65379b7da90b495a9ec177ad0eca4fa5740051
Anton Lykhoyda <lykhoyda@gmail.com>
2023-05-30 13:00:48 +0200

    adjust styles

--- packages/ui/src/components/Header/index.tsx
+++ packages/ui/src/components/Header/index.tsx
@@ -121,14 +121,7 @@ const LinkStyled = styled(Link)(
   color: ${theme.palette.primary.white};
   text-align: center;
   text-decoration: none;
-  background-color: transparent;
-  outline: 0;
-  border: 0;
-  margin: 0;
   cursor: pointer;
-  user-select: none;
-  vertical-align: middle;
-  appearance: none;
   font-weight: 500;
   font-size: 0.875rem;
   line-height: 1.75;
