bc7571debc6c20e27624a1a8a4d58c992812f506
Thibaut Sardan <github@thib.top>
2023-08-10 16:15:53 +0200

    support removeProxies

--- squid/src/util/getOriginAccount.ts
+++ squid/src/util/getOriginAccount.ts
@@ -1,13 +1,15 @@
 import { decodeHex } from '@subsquid/substrate-processor'
 import { encodeId } from './accountEncoding'
+import { encodeAddress } from '@polkadot/util-crypto'
+import { env } from '../main'
 
-export function getOriginAccountId(origin: any): string {
+export function getOriginAccount(origin: any): string {
   if (origin && origin.__kind === 'system' && origin.value.__kind === 'Signed') {
     const id = origin.value.value
     if (id.__kind === 'Id') {
-      return encodeId(decodeHex(id.value))
+      return encodeAddress(encodeId(decodeHex(id.value)), env.prefix)
     } else {
-      return encodeId(decodeHex(id))
+      return encodeAddress(encodeId(decodeHex(id)), env.prefix)
     }
   } else {
     throw new Error('Unexpected origin')
