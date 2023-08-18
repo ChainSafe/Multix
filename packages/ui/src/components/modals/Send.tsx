d383ad07fff90ff20eb2bb473eaed02dbe9b4a9c
Thibaut Sardan <github@thib.top>
2023-08-18 17:04:33 +0100

    remove useless Wanring component

--- packages/ui/src/components/modals/Send.tsx
+++ packages/ui/src/components/modals/Send.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, DialogTitle, Grid, SelectChangeEvent } from '@mui/material'
+import { Alert, Dialog, DialogContent, DialogTitle, Grid, SelectChangeEvent } from '@mui/material'
 import { Button, Select } from '../library'
 import { ReactNode, useCallback, useEffect, useMemo, useState } from 'react'
 import { styled } from '@mui/material/styles'
@@ -14,7 +14,6 @@ import { sortAddresses } from '@polkadot/util-crypto'
 import GenericAccountSelection, { AccountBaseInfo } from '../select/GenericAccountSelection'
 import ManualExtrinsic from '../EasySetup/ManualExtrinsic'
 import BalancesTransfer from '../EasySetup/BalancesTransfer'
-import Warning from '../Warning'
 import { useMultisigProposalNeededFunds } from '../../hooks/useMultisigProposalNeededFunds'
 import { useCheckBalance } from '../../hooks/useCheckBalance'
 import { useGetSubscanLinks } from '../../hooks/useSubscanLink'
@@ -399,7 +398,7 @@ const Send = ({ onClose, className, onSuccess, onFinalized }: Props) => {
                 md={10}
                 className="errorMessage"
               >
-                <Warning text={easyOptionErrorMessage || errorMessage} />
+                <Alert severity="warning">{easyOptionErrorMessage || errorMessage}</Alert>
               </Grid>
             </>
           )}
