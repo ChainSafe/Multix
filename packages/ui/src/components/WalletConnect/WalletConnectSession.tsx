f2066d2671ba64cd18b74f2c4618d0f36a57648f
Thibaut Sardan <github@thib.top>
2023-08-31 17:05:21 +0100

    restrict for non watched

--- packages/ui/src/components/WalletConnect/WalletConnectSession.tsx
+++ packages/ui/src/components/WalletConnect/WalletConnectSession.tsx
@@ -1,12 +1,14 @@
-import { Box, CircularProgress, styled } from '@mui/material'
+import { Alert, CircularProgress, Grid, styled } from '@mui/material'
 import { Button, TextFieldStyled } from '../library'
 import { useCallback, useState } from 'react'
 import { useWalletConnect } from '../../contexts/WalletConnectContext'
+import { useMultiProxy } from '../../contexts/MultiProxyContext'
 
 export const WalletConnectSession = () => {
   const { pair } = useWalletConnect()
   const [uri, setUri] = useState('')
   const [loading, setLoading] = useState(false)
+  const { selectedIsWatched } = useMultiProxy()
 
   const onConnect = useCallback(async () => {
     try {
@@ -25,25 +27,54 @@ export const WalletConnectSession = () => {
   }, [])
 
   return (
-    <BoxStyled>
-      <TextFieldStyled
-        onChange={onUriChange}
-        value={uri}
-        placeholder="WalletConnect key..."
-      />
-      <ButtonStyled
-        disabled={!uri}
-        onClick={onConnect}
+    <Grid
+      container
+      spacing={2}
+    >
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={12}
+        alignItems="center"
       >
-        {loading ? <CircularProgress size={25} /> : 'Connect Dapp'}
-      </ButtonStyled>
-    </BoxStyled>
+        {selectedIsWatched && (
+          <AlertStyled
+            severity="warning"
+            variant="outlined"
+          >
+            Please first select a Pure or Multisig that you are part of, to be able to use it with
+            WalletConnect.
+          </AlertStyled>
+        )}
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={12}
+        alignItems="center"
+      >
+        <TextFieldStyled
+          onChange={onUriChange}
+          value={uri}
+          placeholder="WalletConnect key..."
+          disabled={selectedIsWatched}
+        />
+        <ButtonStyled
+          disabled={!uri || selectedIsWatched}
+          onClick={onConnect}
+        >
+          {loading ? <CircularProgress size={25} /> : 'Connect Dapp'}
+        </ButtonStyled>
+      </Grid>
+    </Grid>
   )
 }
 
-const BoxStyled = styled(Box)`
-  display: flex;
-  align-items: center;
+const AlertStyled = styled(Alert)`
+  margin-right: 0.5rem;
+  border: 0;
 `
 
 const ButtonStyled = styled(Button)`
