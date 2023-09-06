a376c414eca4594f2590fe6eebf0051b723f4172
Thibaut Sardan <github@thib.top>
2023-09-06 17:50:46 +0100

    fix building

--- packages/ui/cypress/support/commands.ts
+++ packages/ui/cypress/support/commands.ts
@@ -1,4 +1,8 @@
+/// <reference types="@testing-library/cypress" />
+/// <reference types="cypress" />
+
 import '@testing-library/cypress/add-commands'
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
