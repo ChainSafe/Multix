6a5bffb1124d8aa74da7e045371eb16330bc6a68
Thibaut Sardan <github@thib.top>
2023-09-06 17:36:24 +0100

    with testing library selectors

--- packages/ui/cypress/support/commands.ts
+++ packages/ui/cypress/support/commands.ts
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+import '@testing-library/cypress/add-commands'
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
--- packages/ui/cypress/support/page-objects/settings.ts
+++ packages/ui/cypress/support/page-objects/settings.ts
@@ -1,4 +1,3 @@
 export const settingsPage = {
-  watchAccountAccountInput: () =>
-    cy.get('[data-cy=watched-account-wrapper] [data-cy=generic-account-selection-input] input')
+  watchAccountAccountInput: () => cy.findByLabelText('Address')
 }
