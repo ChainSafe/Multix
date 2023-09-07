1b7fe4186ab62d2d3481e88655bed44527142e9b
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2023-09-07 13:36:08 +0100

    Support WalletConnect v2 as a wallet (#326)

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -11,6 +11,7 @@ import { AccountNamesContextProvider } from './contexts/AccountNamesContext'
 import { NetworkContextProvider } from './contexts/NetworkContext'
 import MainLayout from './components/layout/Main'
 import { WatchedAddressesContextProvider } from './contexts/WatchedAddressesContext'
+import { WalletConnectContextProvider } from './contexts/WalletConnectContext'
 import { ModalsContextProvider } from './contexts/ModalsContext'
 
 const App = () => {
@@ -27,9 +28,11 @@ const App = () => {
                 <AccountContextProvider>
                   <AccountNamesContextProvider>
                     <MultiProxyContextProvider>
-                      <ModalsContextProvider>
-                        <MainLayout />
-                      </ModalsContextProvider>
+                      <WalletConnectContextProvider>
+                        <ModalsContextProvider>
+                          <MainLayout />
+                        </ModalsContextProvider>
+                      </WalletConnectContextProvider>
                     </MultiProxyContextProvider>
                   </AccountNamesContextProvider>
                 </AccountContextProvider>
