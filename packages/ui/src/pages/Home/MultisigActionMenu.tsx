e6ed51d9bd2125613b0da1604d326b5a04cc7fe2
Thibaut Sardan <github@thib.top>
2023-09-08 14:57:50 +0100

    add link to proxy/multisig

--- packages/ui/src/pages/Home/MultisigActionMenu.tsx
+++ packages/ui/src/pages/Home/MultisigActionMenu.tsx
@@ -5,14 +5,17 @@ import { HiOutlinePencil } from 'react-icons/hi2'
 import { MdOutlineLockReset as LockResetIcon } from 'react-icons/md'
 import { useMultiProxy } from '../../contexts/MultiProxyContext'
 import { useModals } from '../../contexts/ModalsContext'
+import { HiOutlineArrowTopRightOnSquare as LaunchIcon } from 'react-icons/hi2'
+import { useGetSubscanLinks } from '../../hooks/useSubscanLink'
 
 interface MultisigActionMenuProps {
   withSendButton?: boolean
 }
 
 const MultisigActionMenu = ({ withSendButton = true }: MultisigActionMenuProps) => {
-  const { selectedHasProxy, selectedIsWatched } = useMultiProxy()
+  const { selectedHasProxy, selectedIsWatched, selectedMultiProxy } = useMultiProxy()
   const { setIsEditModalOpen, setIsChangeMultiModalOpen, setIsSendModalOpen } = useModals()
+  const { getSubscanAccountLink } = useGetSubscanLinks()
 
   const options: MenuOption[] = useMemo(() => {
     const opts = [
@@ -20,6 +23,16 @@ const MultisigActionMenu = ({ withSendButton = true }: MultisigActionMenuProps) 
         text: 'Edit names',
         icon: <HiOutlinePencil size={20} />,
         onClick: () => setIsEditModalOpen(true)
+      },
+      {
+        text: 'Subscan',
+        icon: <LaunchIcon size={20} />,
+        onClick: () =>
+          window.open(
+            selectedMultiProxy?.proxy
+              ? getSubscanAccountLink(selectedMultiProxy?.proxy)
+              : getSubscanAccountLink(selectedMultiProxy?.multisigs[0].address, true)
+          )
       }
     ]
 
