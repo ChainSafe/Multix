7bbe4b4724fb041c81c015d9bc99a126093ae404
Thibaut Sardan <github@thib.top>
2023-09-13 14:21:27 +0200

    visit settings directly and function to add an account with optional label

--- packages/ui/cypress/fixtures/landingData.ts
+++ packages/ui/cypress/fixtures/landingData.ts
@@ -1,1 +1,4 @@
-export const localHost = 'http://localhost:3333?network=rococo'
+export const baseUrl = 'http://localhost:3333'
+export const networkParams = 'network=rococo'
+export const landingPageUrl = `${baseUrl}?${networkParams}`
+export const settingsPageUrl = `${baseUrl}/settings?${networkParams}`
--- packages/ui/cypress/tests/watched-acccounts/watched-accounts.cy.ts
+++ packages/ui/cypress/tests/watched-acccounts/watched-accounts.cy.ts
@@ -1,15 +1,23 @@
 import { addresses } from '../../fixtures/accounts'
-import { localHost } from '../../fixtures/landingData'
+import { landingPageUrl, settingsPageUrl } from '../../fixtures/landingData'
 import { landingPage } from '../../support/page-objects/landingPage'
 import { settingsPage } from '../../support/page-objects/settingsPage'
 
+const addWatchAccount = (address: string, name?: string) => {
+  settingsPage.accountAddressInput().type(`${address}{enter}`, { delay: 20 })
+
+  if (name) {
+    settingsPage.accountNameInput().type(name)
+  }
+
+  settingsPage.addButton().click()
+}
+
 describe('Watched Accounts', () => {
   it('can add an account to the watch list', () => {
-    cy.visit(localHost)
+    cy.visit(landingPageUrl)
     landingPage.watchAccountButton().click()
-    settingsPage.accountAddressInput().type(`${addresses.Alice}{enter}`, { delay: 20 })
-    settingsPage.accountNameInput().type('Alice')
-    settingsPage.addButton().click()
+    addWatchAccount(addresses.Alice, 'Alice')
     settingsPage.accountContainer().within(() => {
       settingsPage.accountIcon().should('be.visible')
       settingsPage.accountAddressLabel().should('be.visible')
@@ -20,11 +28,8 @@ describe('Watched Accounts', () => {
 
   it('can remove an account from the watch list', () => {
     // add an account first
-    cy.visit(localHost)
-    landingPage.watchAccountButton().click()
-    settingsPage.accountAddressInput().type(`${addresses.Alice}{enter}`, { delay: 20 })
-    settingsPage.accountNameInput().type('Alice')
-    settingsPage.addButton().click()
+    cy.visit(settingsPageUrl)
+    addWatchAccount(addresses.Alice, 'Alice')
     // now remove it
     settingsPage.accountContainer().within(() => {
       settingsPage.accountDeleteButton().click()
@@ -36,25 +41,19 @@ describe('Watched Accounts', () => {
 
   it('can see error when attemping to add same address more than once', () => {
     // add an account first
-    cy.visit(localHost)
-    landingPage.watchAccountButton().click()
-    settingsPage.accountAddressInput().type(`${addresses.Alice}{enter}`, { delay: 20 })
-    settingsPage.accountNameInput().type('Alice')
-    settingsPage.addButton().click()
+    cy.visit(settingsPageUrl)
+    addWatchAccount(addresses.Alice, 'Alice')
     settingsPage.accountContainer().should('have.length', 1)
     // attempt to add the same account again
-    settingsPage.accountAddressInput().type(`${addresses.Alice}{enter}`, { delay: 20 })
-    settingsPage.addButton().click()
+    addWatchAccount(addresses.Alice)
     settingsPage.errorLabel().should('be.visible').should('have.text', 'Account already added')
     settingsPage.accountContainer().should('have.length', 1)
     settingsPage.addButton().should('be.disabled')
   })
 
   it('can see error when attempting to add an invalid address', () => {
-    cy.visit(localHost)
-    landingPage.watchAccountButton().click()
-    settingsPage.accountAddressInput().type('123{enter}')
-    settingsPage.addButton().click()
+    cy.visit(settingsPageUrl)
+    addWatchAccount('123')
     settingsPage.errorLabel().should('be.visible').should('have.text', 'Invalid address')
     settingsPage.accountContainer().should('have.length', 0)
     settingsPage.addButton().should('be.disabled')
