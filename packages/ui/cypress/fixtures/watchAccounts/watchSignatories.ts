2815ed829b9b840b1d9f1a30a8510b786451b76a
Andrew Snaith <andrew@chainsafe.io>
2023-10-06 15:29:54 -0700

    add new test fixtures

---
+++ packages/ui/cypress/fixtures/watchAccounts/watchSignatories.ts
@@ -0,0 +1,29 @@
+export const watchSignatories = [
+  // signatories of watch-multisig-with-pure
+  {
+    address: '5GGjPYsz8B8mxAzNScFNDPkZ1g97VWFCPCMexPSkPnibPBez',
+    name: 'Pure Signatory 1',
+    type: 'sr25519',
+    mnemonic: 'citizen heavy warrior cattle enter chef label split differ seek turtle gorilla'
+  },
+  {
+    address: '5EkbU3anZKYP98aXF5MvmCUxvwvM4kxp7osc2Xhj1wHYL6ym',
+    name: 'Pure Signatory 2',
+    type: 'sr25519',
+    mnemonic: 'script spoon elder spawn kite burst theme property hip fatal flight amount'
+  },
+
+  // signatories of watch-multisig-without-pure
+  {
+    address: '5HfzjVSWj6mxBnqgJhPfUTpkAJKro9BKToxXB3nozbu2MTpV',
+    name: 'No Pure Signatory 1',
+    type: 'sr25519',
+    mnemonic: 'spring banana desert horse ecology resist tag matrix burden heart stereo fix'
+  },
+  {
+    address: '5Df1JyC6KSbjSp3pQEn85PCnvTtknGiN7JyE7bSZ9zqNL76E',
+    name: 'No Pure Signatory 2',
+    type: 'sr25519',
+    mnemonic: 'mutual pluck punch boy gym key brush dune master aunt track dynamic'
+  }
+]
