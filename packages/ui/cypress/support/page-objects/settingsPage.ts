29908c98d948d355859d0b165c40bcc15b0a2d49
Andrew Snaith <asnaith@users.noreply.github.com>
2023-10-12 14:45:42 -0700

    Additional watched account tests (#396)
    Co-authored-by: Thibaut Sardan <github@thib.top>

--- packages/ui/cypress/support/page-objects/settingsPage.ts
+++ packages/ui/cypress/support/page-objects/settingsPage.ts
@@ -3,9 +3,6 @@ export const settingsPage = {
   accountNameInput: () => cy.get('[data-cy=input-account-name]'),
   addButton: () => cy.get('[data-cy=button-add-watched-account]'),
   accountContainer: () => cy.get('[data-cy=container-account-details]', { timeout: 20000 }),
-  accountIcon: () => cy.get('[data-cy=icon-identicon]'),
-  accountNameLabel: () => cy.get('[data-cy=label-account-name]'),
-  accountAddressLabel: () => cy.get('[data-cy=label-account-address]'),
   accountDeleteButton: () => cy.get('[data-cy=button-delete-watched-account]'),
   errorLabel: () => cy.get('[data-cy=label-watch-account-error]')
 }
