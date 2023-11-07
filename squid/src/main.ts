5956da2b614f32202a58d5f403193c0a1b3ea8b9
Thibaut Sardan <github@thib.top>
2023-11-07 12:34:04 +0000

    formating

--- squid/src/main.ts
+++ squid/src/main.ts
@@ -63,13 +63,13 @@ const processor = new SubstrateBatchProcessor()
   .setFields({ ...fields })
   .addCall({
     name: [
-          'Proxy.proxy',
-          'Proxy.remove_proxies',
-          'Multisig.as_multi',
-          'Multisig.approve_as_multi',
-          'Multisig.cancel_as_multi',
-          'Multisig.as_multi_threshold_1'
-        ]
+      'Proxy.proxy',
+      'Proxy.remove_proxies',
+      'Multisig.as_multi',
+      'Multisig.approve_as_multi',
+      'Multisig.cancel_as_multi',
+      'Multisig.as_multi_threshold_1'
+    ]
   })
   .addEvent({
     name: ['Proxy.PureCreated', 'Proxy.AnonymousCreated', 'Proxy.ProxyAdded', 'Proxy.ProxyRemoved']
