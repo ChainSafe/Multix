919cfdbf12ed28894587298c9ccfddf8200177ed
Thibaut Sardan <github@thib.top>
2023-11-22 13:42:36 +0000

    with LEGACY_ASMULTI_PARAM_LENGTH

--- packages/ui/src/utils/getAsMultiTx.ts
+++ packages/ui/src/utils/getAsMultiTx.ts
@@ -12,8 +12,11 @@ interface Params {
   weight?: Weight
   when?: MultisigStorageInfo['when']
 }
+
+const LEGACY_ASMULTI_PARAM_LENGTH = 6
+
 export const getAsMultiTx = ({ api, threshold, otherSignatories, tx, weight, when }: Params) => {
-  return api.tx.multisig.asMulti.meta.args.length === 6
+  return api.tx.multisig.asMulti.meta.args.length === LEGACY_ASMULTI_PARAM_LENGTH
     ? api.tx.multisig.asMulti(threshold, otherSignatories, when || null, tx, false, weight || 0)
     : api.tx.multisig.asMulti(
         threshold,
