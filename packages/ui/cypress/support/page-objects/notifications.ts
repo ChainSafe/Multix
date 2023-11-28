d0822c457b56324afba1ff162b588b06865820e3
Thibaut Sardan <github@thib.top>
2023-11-28 14:34:09 +0000

    take your time for wrapper

--- packages/ui/cypress/support/page-objects/notifications.ts
+++ packages/ui/cypress/support/page-objects/notifications.ts
@@ -2,5 +2,4 @@ export const notifications = {
   successNotificationIcon: () => cy.get('[data-cy=notification-icon-success]'),
   errorNotificationIcon: () => cy.get('[data-cy=notification-icon-error]'),
   loadingNotificationIcon: () => cy.get('[data-cy=notification-icon-loading]', { timeout: 20000 }),
-  notificationWrapper: () => cy.get('[data-cy=notification-wrapper]')
-}
+  notificationWrapper: () => cy.get('[data-cy=notification-wrapper]', { timeout: 20000 })
