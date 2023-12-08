8d86ff8a09e210bc69ca718ad8bcc3594a53709c
Andrew Snaith <andrew@chainsafe.io>
2023-12-08 09:55:56 -0800

    increase timeout of toast for more reliability

--- packages/ui/cypress/support/page-objects/notifications.ts
+++ packages/ui/cypress/support/page-objects/notifications.ts
@@ -1,7 +1,7 @@
 export const notifications = {
-  successNotificationIcon: (timeout = 4000) =>
+  successNotificationIcon: (timeout = 8000) =>
     cy.get('[data-cy=notification-icon-success]', { timeout }),
   errorNotificationIcon: () => cy.get('[data-cy=notification-icon-error]'),
   loadingNotificationIcon: () => cy.get('[data-cy=notification-icon-loading]'),
-  notificationWrapper: (timeout = 4000) => cy.get('[data-cy=notification-wrapper]', { timeout })
+  notificationWrapper: (timeout = 8000) => cy.get('[data-cy=notification-wrapper]', { timeout })
 }
