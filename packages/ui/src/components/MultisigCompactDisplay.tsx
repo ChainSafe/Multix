dae3d3ca79ec744e7cca04060f60851c991bda59
Thibaut Sardan <github@thib.top>
2024-01-20 17:19:22 +0100

    simplify MultisigCompactDisplay

--- packages/ui/src/components/MultisigCompactDisplay.tsx
+++ packages/ui/src/components/MultisigCompactDisplay.tsx
@@ -3,15 +3,8 @@ import { styled } from '@mui/material/styles'
 import { AccountBadge } from '../types'
 import AccountDisplay from './AccountDisplay'
 import Expander from './Expander'
-import {
-  MultisigByIdDocument,
-  MultisigByIdQuery,
-  MultisigByIdQueryVariables
-} from '../../types-and-hooks'
+import { useMultisigByIdQuery } from '../../types-and-hooks'
 import { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { fetchData } from '../fetcher'
-import { useNetwork } from '../contexts/NetworkContext'
 import { useAccountId } from '../hooks/useAccountId'
 
 interface Props {
@@ -22,19 +15,8 @@ interface Props {
 
 const MultisigCompactDisplay = ({ className, address, expanded = false }: Props) => {
   const [signatories, setSignatories] = useState<string[]>([])
-  const { selectedNetworkInfo } = useNetwork()
   const accountId = useAccountId(address)
-  // we can't use the useMultisigById that got generated in types-and-hooks because we need a dynamic url
-  // to fetch for the right network
-  const { data, error, isFetching } = useQuery<MultisigByIdQuery, unknown, MultisigByIdQuery>(
-    ['MultisigById', { id: accountId }],
-    fetchData<MultisigByIdQuery, MultisigByIdQueryVariables>(
-      MultisigByIdDocument,
-      { id: accountId },
-      undefined,
-      selectedNetworkInfo?.httpGraphqlUrl
-    )
-  )
+  const { data, error, isFetching } = useMultisigByIdQuery({ id: accountId })
   const [badge, setBadge] = useState<AccountBadge | undefined>()
   const [threshold, setThreshold] = useState<number | null | undefined>(null)
 
