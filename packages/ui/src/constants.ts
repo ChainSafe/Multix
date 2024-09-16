cc35b44382e85ca73034f2b1162b1444b6636c40
Thibaut Sardan <github@thib.top>
2024-09-16 17:04:40 +0100

    remove rococo

--- packages/ui/src/constants.ts
+++ packages/ui/src/constants.ts
@@ -2,7 +2,6 @@ import { nodesKhalaSVG } from './logos/khalaSVG'
 import { chainsKusamaSVG } from './logos/kusamaSVG '
 import { localSVG } from './logos/localSVG'
 import { chainsPolkadotCircleSVG } from './logos/polkadot-circleSVG'
-import { chainsRococoSVG } from './logos/rococoSVG'
 import { phalaSVG } from './logos/phalaSVG'
 import { nodesAstarPNG } from './logos/astarPNG'
 import { nodesAssetHubSVG } from './logos/assetHubSVG'
@@ -146,13 +145,13 @@ export const networkList: Record<string, NetworkInfo> = {
     httpGraphqlUrl: HTTP_GRAPHQL_URL,
     logo: nodesKhalaSVG
   },
-  rococo: {
-    chainId: 'rococo',
-    explorerNetworkName: 'rococo',
-    rpcUrl: 'wss://rococo-rpc.polkadot.io',
-    httpGraphqlUrl: HTTP_GRAPHQL_URL,
-    logo: chainsRococoSVG
-  },
+  // rococo: {
+  //   chainId: 'rococo',
+  //   explorerNetworkName: 'rococo',
+  //   rpcUrl: 'wss://rococo-rpc.polkadot.io',
+  //   httpGraphqlUrl: HTTP_GRAPHQL_URL,
+  //   logo: chainsRococoSVG
+  // },
   westend: {
     chainId: 'westend',
     explorerNetworkName: 'westend',
@@ -165,7 +164,7 @@ export const networkList: Record<string, NetworkInfo> = {
     chainId: 'paseo',
     explorerNetworkName: 'paseo',
     rpcUrl: 'wss://paseo.rpc.amforc.com',
-    httpGraphqlUrl: 'https://chainsafe.squids.live/multix-arrow/v/v4/graphql',
+    httpGraphqlUrl: HTTP_GRAPHQL_URL,
     logo: paseoSVG
   },
   amplitude: {
@@ -237,7 +236,7 @@ export const soloChains: Partial<keyof typeof networkList>[] = [
   //joystream
 ]
 export const testChains: Partial<keyof typeof networkList>[] = [
-  'rococo',
+  // 'rococo',
   'paseo',
   'rhala testnet',
   'westend',
