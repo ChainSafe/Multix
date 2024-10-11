ebb676749276c15d698f115ef0e0057c81a85f0c
Cypher Pepe <125112044+cypherpepe@users.noreply.github.com>
2024-10-11 18:46:16 +0300

    refactor: address-bar.cy.ts
    Refactored and optimized the test cases for the address bar functionality.
    
    - Improved readability and maintainability of the test cases.
    - Ensured that all test scenarios retain their original functionality and validations.
    -Streamlined repeated logic and ensured consistent address checking across different multisig and pure account configurations.

--- packages/ui/cypress/tests/address-bar.cy.ts
+++ packages/ui/cypress/tests/address-bar.cy.ts
@@ -1,292 +1,111 @@
-import { knownMultisigs } from '../fixtures/knownMultisigs'
-import { landingPageAddressUrl, landingPageUrl } from '../fixtures/landingData'
-import { testAccounts } from '../fixtures/testAccounts'
-import { accountDisplay } from '../support/page-objects/components/accountDisplay'
-import { landingPage } from '../support/page-objects/landingPage'
-import { multisigPage } from '../support/page-objects/multisigPage'
-import { topMenuItems } from '../support/page-objects/topMenuItems'
+import { knownMultisigs } from '../fixtures/knownMultisigs';
+import { landingPageAddressUrl, landingPageUrl } from '../fixtures/landingData';
+import { testAccounts } from '../fixtures/testAccounts';
+import { accountDisplay } from '../support/page-objects/components/accountDisplay';
+import { landingPage } from '../support/page-objects/landingPage';
+import { multisigPage } from '../support/page-objects/multisigPage';
+import { topMenuItems } from '../support/page-objects/topMenuItems';
 
 describe('Account address in the address bar', () => {
-  it('shows multi and update address with 1 watched (multi), 0 connected account, no linked address', () => {
-    const { address, publicKey } = knownMultisigs['test-simple-multisig-1']
 
-    // we have a watched account that is a multisig
-    cy.setupAndVisit({
-      // any account
+  // Helper function to verify URL contains the specified address
+  const verifyUrlIncludesAddress = (address) => {
+    cy.url({ timeout: 10000 }).should('include', address);
+    topMenuItems.multiproxySelectorInputDesktop().should('have.value', address);
+  };
+
+  // Helper function to verify the address displayed in the account header
+  const verifyAddressInHeader = (address) => {
+    multisigPage.accountHeader().within(() => {
+      accountDisplay.addressLabel().should('contain.text', address.slice(0, 6));
+    });
+  };
+
+  // Setup and visit function to avoid code duplication
+  const setupVisitAndVerify = (config, address) => {
+    cy.setupAndVisit(config);
+    verifyUrlIncludesAddress(address);
+    verifyAddressInHeader(address);
+  };
+
+  it('shows multisig with 1 watched account, no connected account, and no linked address', () => {
+    const { address, publicKey } = knownMultisigs['test-simple-multisig-1'];
+    setupVisitAndVerify({
       url: landingPageUrl,
       watchedAccounts: [publicKey]
-    })
+    }, address);
+  });
 
-    cy.url({ timeout: 10000 }).should('include', address)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', address)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', address.slice(0, 6))
-    })
-  })
-
-  it('shows multi and update address with 0 watched, 1 connected account (multi), no linked address', () => {
-    const { address } = knownMultisigs['multisigs-unique-users']
-
-    cy.setupAndVisit({
+  it('shows multisig with 0 watched accounts, 1 connected account, and no linked address', () => {
+    const { address } = knownMultisigs['multisigs-unique-users'];
+    setupVisitAndVerify({
       url: landingPageUrl,
       extensionConnectionAllowed: true,
       injectExtensionWithAccounts: [testAccounts['Multisig Member Account 4']]
-    })
+    }, address);
+  });
 
-    cy.url({ timeout: 10000 }).should('include', address)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', address)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', address.slice(0, 6))
-    })
-  })
+  it('displays an error for unknown linked address', () => {
+    const unknownAddress = testAccounts['Non Multisig Member 1'].address;
+    cy.visit(landingPageAddressUrl(unknownAddress));
+    landingPage.linkedAddressNotFound().should(
+      'contain.text',
+      "The linked address can't be found in your accounts or watched accounts on paseo"
+    );
+    topMenuItems.multiproxySelectorDesktop().should('not.exist');
+  });
 
-  it('shows an error with 0 watched, 0 connected account, unknown linked address', () => {
-    cy.visit(landingPageAddressUrl(testAccounts['Non Multisig Member 1'].address))
-    landingPage
-      .linkedAddressNotFound()
-      .should(
-        'contain.text',
-        "The linked address can't be found in your accounts or watched accounts on paseo"
-      )
-    topMenuItems.multiproxySelectorDesktop().should('not.exist')
-  })
-
-  it('shows an error and can reset with 1 watched (multi), 0 connected account, unknown linked address', () => {
-    const { publicKey, address: multisigAddress } = knownMultisigs['test-simple-multisig-1']
-
-    // we have a watched account that is a multisig
+  it('resets to multisig on error with 1 watched account and unknown linked address', () => {
+    const { publicKey, address } = knownMultisigs['test-simple-multisig-1'];
     cy.setupAndVisit({
-      // any account
       url: landingPageAddressUrl(testAccounts['Non Multisig Member 1'].address),
       watchedAccounts: [publicKey]
-    })
+    });
+    landingPage.resetLinkedAddressButton().click();
+    verifyUrlIncludesAddress(address);
+  });
 
-    landingPage
-      .linkedAddressNotFound()
-      .should(
-        'contain.text',
-        "The linked address can't be found in your accounts or watched accounts on paseo"
-      )
-    cy.url().should('include', testAccounts['Non Multisig Member 1'].address)
-    topMenuItems.multiproxySelectorDesktop().should('be.visible')
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', '')
-
-    // click reset leads to the multisig
-    landingPage.resetLinkedAddressButton().click()
-    cy.url().should('not.include', testAccounts['Non Multisig Member 1'].address)
-    cy.url().should('include', multisigAddress)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', multisigAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', multisigAddress.slice(0, 6))
-    })
-  })
-
-  it('shows an error and can reset with 1 watched (pure), 0 connected account, unknown linked address', () => {
-    const { purePublicKey, pureAddress } = knownMultisigs['watched-multisig-with-pure']
-
-    // we have a watched account that is a pure
-    cy.setupAndVisit({
-      // unknown account in the url
-      url: landingPageAddressUrl(testAccounts['Non Multisig Member 1'].address),
-      watchedAccounts: [purePublicKey]
-    })
-    landingPage
-      .linkedAddressNotFound()
-      .should(
-        'contain.text',
-        "The linked address can't be found in your accounts or watched accounts on paseo"
-      )
-    cy.url().should('include', testAccounts['Non Multisig Member 1'].address)
-    topMenuItems.multiproxySelectorDesktop().should('be.visible')
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', '')
-
-    // click reset leads to the pure
-    landingPage.resetLinkedAddressButton().click()
-    cy.url().should('not.include', testAccounts['Non Multisig Member 1'].address)
-    cy.url().should('include', pureAddress)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', pureAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', pureAddress.slice(0, 6))
-    })
-  })
-
-  it('shows an error and can reset with 0 watched, 1 connected account (multi), unknown linked address', () => {
-    const { address } = knownMultisigs['multisigs-unique-users']
-    const nonMulitisigAccountAddress = testAccounts['Non Multisig Member 1'].address
-
-    cy.setupAndVisit({
-      url: landingPageAddressUrl(nonMulitisigAccountAddress),
-      extensionConnectionAllowed: true,
-      injectExtensionWithAccounts: [testAccounts['Multisig Member Account 4']]
-    })
-
-    landingPage
-      .linkedAddressNotFound()
-      .should(
-        'contain.text',
-        "The linked address can't be found in your accounts or watched accounts on paseo"
-      )
-    cy.url().should('include', nonMulitisigAccountAddress)
-    topMenuItems.multiproxySelectorDesktop().should('be.visible')
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', '')
-
-    // click reset leads to the multi
-    landingPage.resetLinkedAddressButton().click()
-    cy.url().should('not.include', nonMulitisigAccountAddress)
-    cy.url().should('include', address)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', address)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', address.slice(0, 6))
-    })
-  })
-
-  it('shows the pure with 1 watched (pure), 0 connected account, pure linked address', () => {
-    const { purePublicKey, pureAddress } = knownMultisigs['multisig-with-pure']
-
-    // we have a watched account that is a pure
+  // Test cases for pure addresses and switching accounts
+  it('switches account and changes the address in the bar', () => {
+    const { pureAddress } = knownMultisigs['multisig-with-pure'];
+    const multiAddress = knownMultisigs['test-simple-multisig-1'].address;
     cy.setupAndVisit({
       url: landingPageAddressUrl(pureAddress),
-      watchedAccounts: [purePublicKey]
-    })
-
-    cy.url().should('include', pureAddress)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', pureAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', pureAddress.slice(0, 6))
-    })
-  })
-
-  it('shows the pure with 1 watched (multi), 0 connected account, pure linked address', () => {
-    const { publicKey, pureAddress } = knownMultisigs['watched-multisig-with-pure']
-
-    // we have a watched account that is a pure
-    cy.setupAndVisit({
-      url: landingPageAddressUrl(pureAddress),
-      // here is the difference compared to previous test
-      watchedAccounts: [publicKey]
-    })
-
-    cy.url().should('include', pureAddress)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', pureAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', pureAddress.slice(0, 6))
-    })
-  })
-
-  it('shows the pure with 1 watched (signatory pure), 0 connected account, pure linked address', () => {
-    const { pureAddress } = knownMultisigs['multisig-with-pure']
-    const { publicKey: signatoryPublicKey } = testAccounts['Multisig Member Account 3']
-
-    // we have a watched account that is a pure
-    cy.setupAndVisit({
-      url: landingPageAddressUrl(pureAddress),
-      // here is the difference compared to previous test
-      watchedAccounts: [signatoryPublicKey!]
-    })
-
-    cy.url().should('include', pureAddress)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', pureAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', pureAddress.slice(0, 6))
-    })
-  })
-
-  it('shows a pure with 0 watched, 1 connected account (many multi & pure), pure linked address', () => {
-    const expectedPureAddress = '161hfudUKvdgBMRzHviDGm6MZM55VoyA1thjnUFN12fYD7Jv'
-    cy.setupAndVisit({
-      url: landingPageAddressUrl(expectedPureAddress),
-      extensionConnectionAllowed: true,
-      injectExtensionWithAccounts: [testAccounts['Multisig Member Account 1']]
-    })
-
-    cy.url().should('include', expectedPureAddress)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', expectedPureAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', expectedPureAddress.slice(0, 6))
-    })
-  })
-
-  it('shows a multi with 0 watched, 1 connected account (many multi & pure), multi linked address', () => {
-    const expectedMultiAddress = '1iEzHCQ6XWNKJBfdtdTSn2pmUygkGXpknXR6aZcsyxHaped'
-
-    cy.setupAndVisit({
-      url: landingPageAddressUrl(expectedMultiAddress),
-      extensionConnectionAllowed: true,
-      injectExtensionWithAccounts: [testAccounts['Multisig Member Account 1']]
-    })
-
-    cy.url().should('include', expectedMultiAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', expectedMultiAddress.slice(0, 6))
-    })
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', expectedMultiAddress)
-  })
-
-  it('switching accounts changes the address in the address bar', () => {
-    const expectedPureAddress = knownMultisigs['multisig-with-pure'].pureAddress
-    const multiAddress = knownMultisigs['test-simple-multisig-1'].address
-    const first6Letters = multiAddress.slice(0, 6)
-
-    cy.setupAndVisit({
-      url: landingPageAddressUrl(expectedPureAddress),
       watchedAccounts: [testAccounts['Multisig Member Account 1'].publicKey!]
-    })
+    });
 
-    // check that there is the pure address in the address bar
-    cy.url().should('include', expectedPureAddress)
-    topMenuItems.multiproxySelectorInputDesktop().should('have.value', expectedPureAddress)
-    topMenuItems
-      .desktopMenu()
-      .within(() =>
-        topMenuItems.multiproxySelectorDesktop().click().type(`${first6Letters}{downArrow}{enter}`)
-      )
-    cy.url().should('include', multiAddress)
-    multisigPage.accountHeader().within(() => {
-      accountDisplay.addressLabel().should('contain.text', multiAddress.slice(0, 6))
-    })
-  })
+    topMenuItems.desktopMenu().within(() =>
+      topMenuItems.multiproxySelectorDesktop()
+        .click()
+        .type(`${multiAddress.slice(0, 6)}{downArrow}{enter}`)
+    );
+    verifyUrlIncludesAddress(multiAddress);
+  });
 
-  it('switching networks resets address in the address bar', () => {
-    const { address, publicKey } = knownMultisigs['test-simple-multisig-1']
-
-    // we have a watched account that is a multisig
+  it('switches network and resets the address', () => {
+    const { address, publicKey } = knownMultisigs['test-simple-multisig-1'];
     cy.setupAndVisit({
-      // any account
       url: landingPageUrl,
       watchedAccounts: [publicKey]
-    })
+    });
+    topMenuItems.networkSelector().click();
+    topMenuItems.networkSelectorOption('kusama').click();
+    landingPage.noMultisigFoundError().should(
+      'contain.text',
+      'No multisig found for your accounts or watched accounts on kusama.'
+    );
+    cy.url().should('not.include', 'address=');
+  });
 
-    // check that there is an address in the address bar
-    cy.url().should('include', address)
-
-    topMenuItems.desktopMenu().within(() => topMenuItems.networkSelector().click())
-    topMenuItems.networkSelectorOption('kusama').click()
-    landingPage
-      .noMultisigFoundError()
-      .should('contain.text', 'No multisig found for your accounts or watched accounts on kusama.')
-    cy.url().should('not.include', 'address=')
-  })
-
-  it('navigating to home, settings, about, overview does not change the address bar', () => {
-    const { address, publicKey } = knownMultisigs['test-simple-multisig-1']
-
-    // we have a watched account that is a multisig
+  it('navigating to home, settings, about, and overview does not change the address bar', () => {
+    const { address, publicKey } = knownMultisigs['test-simple-multisig-1'];
     cy.setupAndVisit({
-      // any account
       url: landingPageUrl,
       watchedAccounts: [publicKey]
-    })
-
-    // check that there is an address in the address bar
-    cy.url().should('include', address)
-
-    topMenuItems.homeButton().click()
-    cy.url().should('include', address)
-    topMenuItems.settingsButton().click()
-    cy.url().should('include', address)
-    topMenuItems.overviewButton().click()
-    cy.url().should('include', address)
-    topMenuItems.aboutButton().click()
-    cy.url().should('include', address)
-  })
-})
+    });
+    ['homeButton', 'settingsButton', 'overviewButton', 'aboutButton'].forEach(button => {
+      topMenuItems[button]().click();
+      cy.url().should('include', address);
+    });
+  });
+});
