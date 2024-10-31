37e6403037e62d7b992ff79feda3f9ff3fe3c9c1
Thibaut Sardan <github@thib.top>
2024-10-31 18:57:24 +0000

    fix: build

--- packages/ui/cypress/tests/setIdentity.cy.ts
+++ packages/ui/cypress/tests/setIdentity.cy.ts
@@ -82,7 +82,7 @@ describe('Set an identity', () => {
       sendTxModal.buttonSend().should('not.exist')
       sendTxModal.buttonSending().should('be.visible')
       // expected https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Frococo-rpc.polkadot.io#/extrinsics/decode/0x1f0102000412ad770206045069201514711dc2456908b0af226442d475d12a5334e9c4513e001901000564696973056c6565670000000000000000
-      cy.wrap(txRequests[0].payload.method).should(
+      cy.wrap(txRequests[0].payload).should(
         'eq',
         '0x1f0102000412ad770206045069201514711dc2456908b0af226442d475d12a5334e9c4513e001901000564696973056c6565670000000000000000'
       )
